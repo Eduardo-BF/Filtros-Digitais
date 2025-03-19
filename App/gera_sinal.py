22dc1bb7f5805adb73dc1c1dc368fa8a3cd70591
Eduardo Barcelos <eduardobarcelosfreitas@hotmail.com>
2025-03-18 21:59:58 -0300

    add convo. low/high-pass filter

--- App/gera_sinal.py
+++ App/gera_sinal.py
@@ -2,15 +2,16 @@ import numpy as np
 import pandas as pd
 
 # Parâmetros do sinal
-fs = 200  # Frequência de amostragem (Hz)
-t = np.arange(0, 1, 1/fs)  # Tempo de 1 segundo
+fs = 200    # Frequência de amostragem (Hz)
+dt = 1      # Duração de 1 segundo
+t = np.arange(0, dt, 1/fs)  
 
 # Frequências do sinal
-f1 = 2  # Frequência de 2 Hz
-f2 = 20  # Frequência de 20 Hz
+f1 = 2  # Hz
+f2 = 20 # Hz
 
 # Gerar o sinal (união das duas frequências)
-signal = np.sin(2 * np.pi * f1 * t) + np.sin(2 * np.pi * f2 * t)
+signal = np.sin(2 * np.pi * f1 * t) + 0,5*np.sin(2 * np.pi * f2 * t)
 
 # Criar o DataFrame para salvar em CSV
 df = pd.DataFrame({
@@ -19,7 +20,6 @@ df = pd.DataFrame({
 })
 
 # Salvar o DataFrame em um arquivo CSV
-csv_path = 'Dados/sinal_c.csv'
+csv_path = '../Dados/sinal.csv'
 df.to_csv(csv_path, index=False)
-
-csv_path  # Retornar o caminho do arquivo gerado
+csv_path  
